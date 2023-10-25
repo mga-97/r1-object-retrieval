73bf942c51fb7fef43d948a78508b2a12a1f7467
Raffaele Colombo <colombraf@gmail.com>
2023-10-25 17:43:35 +0200

    small fix

--- docker/docker_r1ObjectRetrieval/build_docker_obj_retr.sh
+++ docker/docker_r1ObjectRetrieval/build_docker_obj_retr.sh
@@ -3,4 +3,4 @@
 ROS_DISTRO="iron"
 IMAGE_NAME="colombraf/r1images:r1ObjectRetrieval"
 
-sudo docker build --build-arg ros_distro=$ROS_DISTRO -t $IMAGE_NAME .
+sudo docker build $1 --build-arg ros_distro=$ROS_DISTRO -t $IMAGE_NAME .
