947849fb522e4260cd541ceb7bff3e5d12dd9bc8
Raffaele Colombo <colombraf@gmail.com>
2023-11-14 15:52:55 +0100

    added chatGPT filter

--- modules/sensorNetworkReceiver/main.cpp
+++ modules/sensorNetworkReceiver/main.cpp
@@ -25,11 +25,15 @@
 #include <yarp/os/Time.h>
 #include <yarp/os/Port.h>
 #include <yarp/os/RFModule.h>
+#include <yarp/os/RpcClient.h>
 #include <yarp/os/BufferedPort.h>
+#include <yarp/dev/ILLM.h>
+#include <yarp/dev/PolyDriver.h>
 
 YARP_LOG_COMPONENT(SENSOR_NETWORK_RECEIVER, "r1_obr.sensorNetworkReceiver")
 
 using namespace yarp::os;
+using namespace yarp::dev;
 using namespace std;
 
 class sensorNetworkReceiver : public RFModule
@@ -47,6 +51,17 @@ private:
 
     BufferedPort<Bottle> m_output_port;
     string               m_output_port_name;
+
+    RpcClient            m_rpc_to_orchestrator_port;
+    string               m_rpc_to_orchestrator_port_name;
+
+    //ChatGPT Filter
+    bool                 m_llm_active;
+    PolyDriver           m_poly;
+    Property             m_prop;
+    string               m_llm_local;
+    string               m_llm_remote;
+    ILLM*                m_iLlm = nullptr;
 
 public:
 
@@ -71,14 +86,17 @@ sensorNetworkReceiver::sensorNetworkReceiver()
     m_rpc_server_port_name  = "/sensorNetworkReceiver/rpc";
     m_input_port_name = "/sensorNetworkReceiver/input:i";
     m_output_port_name = "/sensorNetworkReceiver/output:o";
+    m_llm_active = false;
+    m_llm_local = "/sensorNetworkReceiver/llm/rpc";
+    m_llm_remote = "/yarpgpt/rpc";
+    m_rpc_to_orchestrator_port_name = "/sensorNetworkReceiver/orchestrator:rpc";
 };
 
 bool sensorNetworkReceiver::configure(ResourceFinder &rf)
 {
     if(rf.check("period")){m_period = rf.find("period").asFloat32();}   
     
-    if(rf.check("rpc_port"))
-        m_rpc_server_port_name = rf.find("rpc_port").asString();
+    if(rf.check("rpc_port")) { m_rpc_server_port_name = rf.find("rpc_port").asString();}
     if (!m_rpc_server_port.open(m_rpc_server_port_name))
     {
         yCError(SENSOR_NETWORK_RECEIVER, "Could not open rpc port %s, check network", m_rpc_server_port_name.c_str());
@@ -90,20 +108,46 @@ bool sensorNetworkReceiver::configure(ResourceFinder &rf)
         return false;
     }
 
-    if(rf.check("input_port"))
-        m_input_port_name = rf.find("input_port").asString();
+    if(rf.check("input_port")) { m_input_port_name = rf.find("input_port").asString();}
     if (!m_input_port.open(m_input_port_name))
     {
         yCError(SENSOR_NETWORK_RECEIVER, "Unable to open input port");
         return false;
     }
 
-    if(rf.check("output_port"))
-        m_output_port_name = rf.find("output_port").asString();
+    if(rf.check("output_port")) {m_output_port_name = rf.find("output_port").asString();}
     if (!m_output_port.open(m_output_port_name))
     {
         yCError(SENSOR_NETWORK_RECEIVER, "Unable to open output port");
         return false;
+    }
+
+    
+    if(rf.check("llm_active")) { m_llm_active = (rf.find("llm_active").asString() == "true");}
+    if (m_llm_active)
+    {
+        if(rf.check("llm_local")) { m_llm_local = rf.find("llm_local").asString();}
+        if(rf.check("llm_remote")) { m_llm_remote = rf.find("llm_remote").asString();}
+        m_prop.put("device", "LLM_nwc_yarp");
+        m_prop.put("local", m_llm_local);
+        m_prop.put("remote", m_llm_remote);
+        if (!m_poly.open(m_prop)) {
+            yCWarning(SENSOR_NETWORK_RECEIVER) << "Cannot open LLM_nwc_yarp";
+            return false;
+        }
+
+        if (!m_poly.view(m_iLlm)) {
+            yCWarning(SENSOR_NETWORK_RECEIVER) << "Cannot open interface from driver";
+            return false;
+        }
+
+        if(rf.check("rpc_to_orchestrator")) {m_rpc_to_orchestrator_port_name = rf.find("rpc_to_orchestrator").asString();}
+        if (!m_rpc_to_orchestrator_port.open(m_rpc_to_orchestrator_port_name))
+        {
+            yCError(SENSOR_NETWORK_RECEIVER, "Unable to open RPC port to orchestrator");
+            return false;
+        }
+
     }
 
     return true;
@@ -121,6 +165,20 @@ bool sensorNetworkReceiver::respond(const Bottle &b, Bottle &reply)
         reply.addString("Everything you will send to this port will be forwarded to the port " + m_output_port_name + ". The reply is forwarded from port " + m_input_port_name);
         return true;
     }
+    
+    if(m_llm_active)
+    {
+        string answer;
+        m_iLlm->ask(b.toString(), answer);
+        Bottle btl; btl.fromString(answer);
+        if (btl.get(0).asString()=="say")
+        {
+            m_rpc_to_orchestrator_port.write(btl);
+            reply.addString("Sto parlando");
+            return true;
+        }
+    }
+  
 
     Bottle&  out = m_output_port.prepare();
     out.clear();
@@ -130,13 +188,15 @@ bool sensorNetworkReceiver::respond(const Bottle &b, Bottle &reply)
     bool reply_arrived{false};
     while (!reply_arrived) 
     {
-        Bottle* reply_bottle = m_input_port.read(false);
-        if (reply_bottle)
+        Bottle* reply_ptr = m_input_port.read(false);
+        if (reply_ptr)
         {
             reply_arrived = true;
-            reply = *reply_bottle;
+            reply = *reply_ptr;
         }
-        Time::delay(0.1);                    
+        else
+            Time::delay(0.5); 
+                           
     } 
     
     if (reply.size()==0)
@@ -166,6 +226,12 @@ bool sensorNetworkReceiver::close()
     
     if (!m_output_port.isClosed())
         m_output_port.close();
+
+    if (m_rpc_to_orchestrator_port.asPort().isOpen())
+        m_rpc_to_orchestrator_port.close(); 
+    
+    if(m_poly.isValid())
+        m_poly.close();
 
     return true;
 }
